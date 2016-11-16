78c29a342ada70007ee75e9e6e12b21c8215b38a
Brent Pitman <bpitman@Brents-MacBook-Pro.local>
2016-11-16 10:03:06 -0800

    Add support for multiple realms

---
+++ app/com/linkedin/drelephant/RealmContext.scala
@@ -0,0 +1,24 @@
+package com.linkedin.drelephant
+
+class RealmContext extends play.mvc.PathBindable[RealmContext] {
+  def context: ElephantContext = null
+
+  def bind(key: String, name: String): RealmContext = {
+    val context = ElephantContext.instance(name)
+    if (context == null)
+      throw new IllegalArgumentException(s"realm [${name}] not found")
+    new NamedRealmContext(name, context)
+  }
+
+  def javascriptUnbind(): String = null
+
+  def unbind(key: String): String = {
+    throw new UnsupportedOperationException("Cannot unbind generic context")
+  }
+}
+
+class NamedRealmContext(val name: String, override val context: ElephantContext) extends RealmContext {
+  override def unbind(key: String): String = {
+    name
+  }
+}
